45c26574edef9024a52aa8cf96a018dfe84454a1
JulyK9 <jkesonjk@gmail.com>
2022-12-12 00:47:20 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -1,23 +1,16 @@
 function solution(a, b) {
-    // let arr = [a, b]
-    // arr.sort((a, b) => a - b);
-    let sum = 0
+//     let sum = 0
     
-    if (a === b) return a;
+//     if (a === b) return a;
     
-    // if (a - b > 0) {
-    //     for (let i = b; i < a; i++) {
-    //         sum = sum + i
-    //     }
-    //         return sum
-    // }
-    if (a - b > 0) {
-            for (let i = b; i <= a; i++) {
-            sum = sum + i
-        } return sum    
-    } else {
-        for (let i = a; i <= b; i++) {
-            sum = sum + i
-        } return sum    
-    }
+//     if (a - b > 0) {
+//             for (let i = b; i <= a; i++) {
+//             sum = sum + i
+//         } return sum    
+//     } else {
+//         for (let i = a; i <= b; i++) {
+//             sum = sum + i
+//         } return sum    
+//     }
+    return (Math.abs(a - b) + 1) * (a + b) / 2;
 }
