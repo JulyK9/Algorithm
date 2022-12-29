626696152a33534d8dc7888362eaa39a7447c1b2
JulyK9 <jkesonjk@gmail.com>
2022-12-29 13:19:41 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,14 +1,3 @@
 function solution(s) {
-    
-    const ctrIdx = Math.floor(s.length / 2)
-    
-    // 단어길이 홀수
-    if(s.length % 2 === 1) {
-      return s[ctrIdx]
-    } 
-  
-    // 단어길이 짝수
-    else {
-      return s[ctrIdx-1] + s[ctrIdx]
-    }  
+    return s.substring(Math.ceil(s.length / 2) - 1, Math.floor(s.length / 2) + 1);
 }
